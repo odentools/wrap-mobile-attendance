5e9d23779db0ec2a13ce6da780608d2c109a30d1
yoshida <ht12a101@oecu.jp>
2015-09-30 14:32:39 +0900

    update
    順番を変えた

--- wrapper.js
+++ wrapper.js
@@ -7,54 +7,6 @@ var app = express();
 app.use(bodyParser());
 app.use(express.static('www'));
 
-app.post('/table', function(req, res){
-  res.writeHead(200, {"Content-Type": "text/html","Access-Control-Allow-Origin":"*"});
-  
-  var json = req.body;
-  json.refine.push({key:"状態", val:"履修", com: "=="});
-  sjson.refine.push({key:"", val:"集中講義", com: "!in"});
-  var options = {
-    uri: 'http://localhost:8080/refine',
-    form: json,
-    //json: true
-  };
-  var ki = ['前期', '後期'];
-  var week = ['月曜日', '火曜日', '水曜日', '木曜日', '金曜日', '土曜日'];
-  var period = ['1', '2', '3', '4', '5', '6', '7'];
-
-  request.post(options, function(error, response, body){
-    if (!error && response.statusCode == 200) {
-      var bodyJson = JSON.parse(body);
-
-      var json = {};
-      for ( var i = 0; i < ki.length; i++) {
-        var tmpKi = {};
-        for ( var j = 0; j < week.length; j++) {
-          var tmpWeek = {};
-          for ( var k = 0; k < period.length; k++) {
-            tmpWeek[period[k]] = {};
-          }
-          tmpKi[week[j]] = tmpWeek;
-        }
-      json[ki[i]] = tmpKi;
-      }
-
-      
-      for ( var l = 0; l < bodyJson.lesson.length; l++) {
-        var tmpLesson = bodyJson.lesson[l];
-        json[tmpLesson['期']][tmpLesson['曜日']][tmpLesson['時限']] = tmpLesson;
-      }
-
-      res.write(JSON.stringify(json))
-
-      res.end();
-    } else {
-      //console.log('error: '+ response.statusCode);
-      res.end();
-    }
-  });
-});
-
 app.post('/refine', function(req, res){
   res.writeHead(200, {"Content-Type": "text/html","Access-Control-Allow-Origin":"*"});
 
@@ -71,11 +23,11 @@ app.post('/refine', function(req, res){
     if (!error && response.statusCode == 200) {
       json = body;
       if( req.body.refine != null && json.lesson != null){
-        G:for(var i = 0; i < json.lesson.length; i++){
-          for ( var j = 0; j < req.body.refine.length; j++) {
+        G:for (var i = 0; i < json.lesson.length; i++){
+          for (var j = 0; j < req.body.refine.length; j++) {
             switch(req.body.refine[j]['com']){
               case('=='):
-                if(json.lesson[i][req.body.refine[j]['key']] != req.body.refine[j]['val']){
+                if (json.lesson[i][req.body.refine[j]['key']] != req.body.refine[j]['val']){
                   json.lesson.splice(i,1);
                   i--;
                   continue G;
@@ -83,7 +35,7 @@ app.post('/refine', function(req, res){
                 break;
 
               case('!='):
-                if(json.lesson[i][req.body.refine[j]['key']] == req.body.refine[j]['val']){
+                if (json.lesson[i][req.body.refine[j]['key']] == req.body.refine[j]['val']){
                   json.lesson.splice(i,1);
                   i--;
                   continue G;
@@ -91,7 +43,7 @@ app.post('/refine', function(req, res){
                 break;
 
               case('in'):
-                if(json.lesson[i][req.body.refine[j]['key']].indexOf(req.body.refine[j]['val'] ) == -1){
+                if (json.lesson[i][req.body.refine[j]['key']].indexOf(req.body.refine[j]['val'] ) == -1){
                   json.lesson.splice(i,1);
                   i--;
                   continue G;
@@ -99,7 +51,7 @@ app.post('/refine', function(req, res){
                 break;
 
               case('!in'):
-                if(json.lesson[i][req.body.refine[j]['key']].indexOf(req.body.refine[j]['val'] ) != -1){
+                if (json.lesson[i][req.body.refine[j]['key']].indexOf(req.body.refine[j]['val'] ) != -1){
                   json.lesson.splice(i,1);
                   i--;
                   continue G;
@@ -108,7 +60,7 @@ app.post('/refine', function(req, res){
 
               case('=>'):
               case('>='):
-                if(parseInt(json.lesson[i][req.body.refine[j]['key']]) <= parseInt(req.body.refine[j]['val'])){
+                if (parseInt(json.lesson[i][req.body.refine[j]['key']]) <= parseInt(req.body.refine[j]['val'])){
                   json.lesson.splice(i,1);
                   i--;
                   continue G;
@@ -117,7 +69,7 @@ app.post('/refine', function(req, res){
 
               case('=<'):
               case('<='):
-                if(parseInt(json.lesson[i][req.body.refine[j]['key']]) >= parseInt(req.body.refine[j]['val'])){
+                if (parseInt(json.lesson[i][req.body.refine[j]['key']]) >= parseInt(req.body.refine[j]['val'])){
                   json.lesson.splice(i,1);
                   i--;
                   continue G;
@@ -125,14 +77,14 @@ app.post('/refine', function(req, res){
                 break;
 
               case('>'):
-                if(parseInt(json.lesson[i][req.body.refine[j]['key']]) < parseInt(req.body.refine[j]['val'])){
+                if (parseInt(json.lesson[i][req.body.refine[j]['key']]) < parseInt(req.body.refine[j]['val'])){
                   json.lesson.splice(i,1);
                   i--;
                   continue G;
                 }
                 break;
               case('<'):
-                if(parseInt(json.lesson[i][req.body.refine[j]['key']]) > parseInt(req.body.refine[j]['val'])){
+                if (parseInt(json.lesson[i][req.body.refine[j]['key']]) > parseInt(req.body.refine[j]['val'])){
                   json.lesson.splice(i,1);
                   i--;
                   continue G;
@@ -147,7 +99,56 @@ app.post('/refine', function(req, res){
 
       res.write(JSON.stringify(json));
       res.end();
+    } else {
+      //console.log('error: '+ response.statusCode);
+      res.end();
+    }
+  })
+});
 
+app.post('/table', function(req, res){
+  res.writeHead(200, {"Content-Type": "text/html","Access-Control-Allow-Origin":"*"});
+
+  // 届いたデータを表示
+  //console.log(req.body);
+
+  var json = req.body;
+  json.refine.push({key: "状態", val: "履修", com: "=="});
+  json.refine.push({key: "", val: "集中講義", com: "!in"});
+  var options = {
+    uri: 'http://localhost:8080/refine',
+    form: json,
+    //json: true
+  };
+  var ki = ['前期', '後期'];
+  var week = ['月曜日', '火曜日', '水曜日', '木曜日', '金曜日', '土曜日'];
+  var period = ['1', '2', '3', '4', '5', '6', '7'];
+
+  request.post(options, function(error, response, body){
+    if (!error && response.statusCode == 200) {
+      var bodyJson = JSON.parse(body);
+
+      var json = {};
+      for (var i = 0; i < ki.length; i++) {
+        var tmpKi = {};
+        for (var j = 0; j < week.length; j++) {
+          var tmpWeek = {};
+          for (var k = 0; k < period.length; k++) {
+            tmpWeek[period[k]] = {};
+          }
+          tmpKi[week[j]] = tmpWeek;
+        }
+      json[ki[i]] = tmpKi;
+      }
+
+      
+      for (var l = 0; l < bodyJson.lesson.length; l++) {
+        var tmpLesson = bodyJson.lesson[l];
+        json[tmpLesson['期']][tmpLesson['曜日']][tmpLesson['時限']] = tmpLesson;
+      }
+
+      res.write(JSON.stringify(json))
+      res.end();
     } else {
       //console.log('error: '+ response.statusCode);
       res.end();
